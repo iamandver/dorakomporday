26210b85e1629aef282858fc5e7b89d0bf75c8c8
Andras Vermes <a.vermes@gmail.com>
2026-02-10 18:26:00 +0100

    new resource

--- src/js/database.js
+++ src/js/database.js
@@ -23,4 +23,16 @@ export const PORTFOLIO = [
             href: "assets/portfolio-content/turn-objections-into-opportunities/index.html"
         }
     },
+    {
+        id         : "video-01",
+        title      : "Lorem ipsum dolor sit amet",
+        type       : "Video",
+        description: "Short description shown on the preview page.",
+        coverImage : "assets/images/dora_picture.png",
+
+        launch: {
+            kind: "video", // "link" | "image" | "video" | "embed"
+            href: "assets/portfolio-content/turn-objections-into-opportunities/index.html"
+        }
+    },
 ];
